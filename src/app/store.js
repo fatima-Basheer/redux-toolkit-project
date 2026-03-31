7d4e96f32e239bf4af2855943ebc9129853fe74b
fatima-Basheer <fatimabasheer8214@gmail.com>
2026-03-31 07:15:01 -0700

    Add files via upload

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import todosReducer from "/src/features/todos/todosSlice";
+
+export const store = configureStore({
+  reducer: {
+    todos: todosReducer,
+  },
+});
